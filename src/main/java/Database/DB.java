e37f943f7ca2a9e323b6e5bd16c973deea30c834
Tóth János Donát <ttdoni99@gmail.com>
2021-05-13 22:58:25 +0200

    I added javadoc in DB.java.

--- src/main/java/Database/DB.java
+++ src/main/java/Database/DB.java
@@ -8,6 +8,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Generic for database operations.
+ */
 public final class DB {
 
     private static Logger logger = LoggerFactory.getLogger("DB.class");
@@ -17,10 +20,19 @@ public final class DB {
 
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
 
+    /**
+     * Method for creating an entity manager anywhere in the program.
+     * @return new entity manager.
+     */
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
 
+    /**
+     * Generic method for committing changes to database Entities.
+     * @param entity to change.
+     * @param <T> Entity that extends the TypeInterface.
+     */
     public static <T extends TypeInterface> void commitChange(T entity) {
         EntityManager em = getEntityManager();
         try {
@@ -35,6 +47,11 @@ public final class DB {
         }
     }
 
+    /**
+     *Generic method for creating new Entities in the database.
+     * @param entity to change.
+     * @param <T> Entity that extends the TypeInterface.
+     */
     public static <T extends TypeInterface> void uploadEntityToDatabase(T entity) {
         EntityManager em = DB.getEntityManager();
         try {
@@ -50,6 +67,11 @@ public final class DB {
         }
     }
 
+    /**
+     * Generic method for deleting Entities form the database.
+     * @param entity to delete.
+     * @param <T> Entity that extends the TypeInterface.
+     */
     public static <T extends TypeInterface> void removeEntity(T entity) {
         EntityManager em = getEntityManager();
         try {
@@ -65,6 +87,9 @@ public final class DB {
         }
     }
 
+    /**
+     * Method to close the emf.
+     */
     public static void closeEmf() {
 
         emf.close();
